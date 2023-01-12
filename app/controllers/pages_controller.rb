747c5f47bdb1d51db9e407a12a18d18619f9f1b0
mariusisoardi <misoardi13100@outlookf.fr$$$>
2023-01-12 12:01:00 +0100

    initial commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
-class PagesController < ApplicationController
-  def salut
 
+class PagesController < ApplicationController
+  # ON RECUPERE LE PARAMS NAME DANS LE CONTROLLERS SALUT
+  def salut
+    @name = params[:name]
   end
+
+  def home; end
 end
